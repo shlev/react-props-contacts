f76c4c8a07aea3d17c2e11734be6159f31acc009
shlomi <shl.lev.ks@gmail.com>
2021-07-13 13:52:54 +0300

    extract components

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
+import Avatar from './Avatar'
+import Detail from './Detail';
 
 function Card(props) {
     console.log(props);
     return (
       <div className="card">
         <div className="top">
-          <h2 className="name">{props.name}</h2>
-          <img
-            className="circle-img"
-            src={props.imgUrl}
-            alt="avatar_img"
-          />
+            <h2 className="name">{props.name}</h2>
+            <Avatar img = {props.imgUrl}/>
         </div>
         <div className="bottom">
-          <p className="info">{props.phone}</p>
-          <p className="info">{props.email}</p>
+            <Detail detailInfo={props.phone} />
+            <Detail detailInfo={props.email} />
         </div>
       </div>
     );
